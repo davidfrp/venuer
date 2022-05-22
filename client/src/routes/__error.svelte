5eaf47336bc4aa80f5b22a5f04b6488655ee2371
davidfrp <davidfrp@outlook.dk>
2022-05-22 17:26:30 +0200

    Switched to SvelteKit

---
+++ client/src/routes/__error.svelte
@@ -0,0 +1,13 @@
+<script context="module" lang="ts">
+  import type { Load } from '@sveltejs/kit'
+
+  export const load: Load = async ({ error }) => ({ props: { error } })
+</script>
+
+<script lang="ts">
+  export let error: Error
+</script>
+
+<h1>An error occurred</h1>
+<pre>{JSON.stringify(error, null, 2)}</pre>
+<p>{error.message}</p>
