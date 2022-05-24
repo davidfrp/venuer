2ef753231732b55f21e0ab36a77069bf734d06fa
davidfrp <davidfrp@outlook.dk>
2022-05-24 14:40:50 +0200

    Removed Apollo references

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,71 +1,28 @@
 import 'dotenv/config'
-import { ApolloServer } from 'apollo-server-express'
-import { ApolloServerPluginDrainHttpServer } from 'apollo-server-core'
-import { schema } from './graphql/schemaBuilder'
-import authContext from './authContext'
-import { connect } from 'mongoose'
-import express from 'express'
-import http from 'http'
+import express, { RequestHandler } from 'express'
+import authRouter from './routers/auth.router'
+import userRouter from './routers/user.router'
+import mongoose from 'mongoose'
 
-import { Location } from './models/location.model'
-import { Venue } from './models/venue.model'
-import { User } from './models/user.model'
+const uri = process.env.MONGODB_URI as string
+mongoose.connect(uri)
 
-startApolloServer()
+const app = express()
 
-async function startApolloServer () {
-  connect(process.env.MONGO_URI as string)
-
-  // const user = new User({
-  //   name: 'Heather',
-  //   email: 'heather@bbc.co.uk',
-  //   password: '12345678Aa'
-  // })
-
-  // await user.save()
-
-  // const location = new Location({
-  //   country: 'Denmark',
-  //   city: 'Copenhagen',
-  //   postalCode: '2300',
-  //   streetName: 'Ørestads Blvd.',
-  //   streetNumber: '13',
-  //   entrance: 'Gadeplan',
-  //   entranceCoordinates: {
-  //     latitude: 55.657490,
-  //     longitude: 12.589098
-  //   }
-  // })
-
-  // location.save()
-
-  // const venue = new Venue({
-  //   owner: user,
-  //   name: 'Hollywood Galla',
-  //   description: 'DR Koncerthuset ruller den røde løber ud til en funklende Hollywood-galla med DR Symfoniorkestret, DR Juniorkoret, Andrea Oehlenschlæger, Diluckshan Jeyaratnam og vært Ane Cortzen i centrum for et musikalsk festfyrværkeri fra de bedste Hollywood-film som ”Pirates of the Caribbean”, ”Braveheart” – og mange flere.',
-  //   location
-  // })
-
-  // await venue.save()
-
-  // const user = await User.findOne()
-  // const location = await Location.findOne()
-  const venue = await Venue.findOne({
-    name: 'Hollywood Galla'
-  })
-
-  console.log(venue)
-
-  const app = express()
-  const httpServer = http.createServer(app)
-  const server = new ApolloServer({
-    schema,
-    csrfPrevention: true,
-    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
-    context: authContext
-  })
-  await server.start()
-  server.applyMiddleware({ app })
-  await new Promise<void>(resolve => httpServer.listen({ port: 4000 }, resolve))
-  console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
+const allowCors: RequestHandler = (_, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3000')
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+  next()
 }
+
+app.use(allowCors)
+app.use(express.json())
+
+app.use('/auth', authRouter)
+app.use('/users', userRouter)
+
+const PORT = process.env.PORT || 4000
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server ready at http://localhost:${PORT}`)
+})
