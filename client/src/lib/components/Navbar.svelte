b64c7317a1fa97d36af62b9d9962f486a134c137
davidfrp <davidfrp@outlook.dk>
2022-06-08 22:37:17 +0200

    Added prefetch attribute to navbar links

--- client/src/lib/components/Navbar.svelte
+++ client/src/lib/components/Navbar.svelte
@@ -6,16 +6,16 @@
 
 <nav class="fixed z-10 w-full h-20 flex items-center justify-between px-4 border-b bg-white">
   <div class="flex-1">
-    <a sveltekit:prefetch href="/" class="px-3 py-2 bg-green-500 bg-opacity-10">Venuer</a>
+    <a sveltekit:prefetch href="/" class="no-base px-3 py-2 hover:underline">Venuer</a>
   </div>
   <Searchbar />
   <div class="flex-1 flex justify-end">
     {#if $session.user}
-      <a href="/account-settings" class="px-3 py-2 bg-green-500 bg-opacity-10">Your account</a>
-      <button on:click={logout} class="px-3 py-2 bg-green-500 bg-opacity-10">Log out</button>
+      <a href="/me" class="no-base px-3 py-2 hover:underline">Your account</a>
+      <button on:click={logout} class="px-3 py-2 hover:underline">Log out</button>
     {:else}
-      <a href="/login" class="px-3 py-2 bg-green-500 bg-opacity-10">Log in</a>
-      <a href="/register" class="px-3 py-2 bg-green-500 bg-opacity-10">Sign up</a>
+      <a href="/login" class="no-base px-3 py-2 hover:underline">Log in</a>
+      <a href="/register" class="no-base px-3 py-2 hover:underline">Sign up</a>
     {/if}
   </div>
 </nav>
