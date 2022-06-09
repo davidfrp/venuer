1e0a011ea48657fbbb1f092a09ba5199cc4b4116
davidfrp <davidfrp@outlook.dk>
2022-06-09 11:03:40 +0200

    Extracted venue listing into its own component

---
+++ client/src/routes/me/organizing/_VenueListing.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  import Button from '$lib/components/Button.svelte'
+  import Modal from '$lib/components/Modal.svelte'
+  import SaveVenueForm from './_SaveVenueForm.svelte'
+  import DeleteVenueForm from './_DeleteVenueForm.svelte'
+
+  export let venue: Venue
+
+  let isSavingModalShown = false
+  let isDeletingModalShown = false
+</script>
+
+<div class="self-start flex gap-3 items-center w-full py-6">
+  <span class="truncate font-semibold">{venue.name}</span>
+  <a class="flex-1" href="/{venue.slug}">Go to venue page</a>
+  <Button variant="text" on:click={() => isSavingModalShown = true}>Edit</Button>
+  <Button variant="text" on:click={() => isDeletingModalShown = true}>Delete</Button>
+</div>
+
+<Modal title="Edit venue" isOpen={isSavingModalShown} onRequestClose={() => isSavingModalShown = false}>
+  <SaveVenueForm {venue} onSaved={() => isSavingModalShown = false} />
+</Modal>
+
+<Modal title="Delete venue" isOpen={isDeletingModalShown} onRequestClose={() => isDeletingModalShown = false}>
+  <DeleteVenueForm {venue} onDeleted={() => isDeletingModalShown = false} />
+</Modal>
--- client/src/routes/me/organizing/index.svelte
+++ client/src/routes/me/organizing/index.svelte
@@ -13,65 +13,31 @@
 </script>
 
 <script lang="ts">
+  import VenueListing from './_VenueListing.svelte'
   import IconButton from '$lib/components/IconButton.svelte'
-  import Button from '$lib/components/Button.svelte'
   import Modal from '$lib/components/Modal.svelte'
   import SaveVenueForm from './_SaveVenueForm.svelte'
-  import DeleteVenueForm from './_DeleteVenueForm.svelte'
 
   export let venues: Venue[]
 
-  let isSavingVenue: boolean
-  let isDeletingVenue: boolean
-  
-  let venueToModify: Venue | undefined
-
-  // Once modals are closed, reset venueToModify to undefined.
-  $: venueToModify = isSavingVenue || isDeletingVenue ? venueToModify : undefined
+  let isSavingModalShown: boolean
 </script>
 
 <div class="flex flex-col gap-6 items-center pt-6">
   <div class="w-full divide-y">
     {#each venues as venue}
-      <div class="self-start flex gap-3 items-center w-full py-6">
-        <span class="truncate font-semibold text-lg">{venue.name}</span>
-        <a class="flex-1" href="/{venue.slug}">Go to venue page</a>
-        <Button
-          on:click={() => {
-            venueToModify = venue
-            isSavingVenue = true
-          }}
-          variant="text"
-        >Edit</Button>
-        <Button
-          variant="text"
-          on:click={() => {
-            venueToModify = venue
-            isDeletingVenue = true
-          }}
-        >Delete</Button>
-      </div>
+      <VenueListing {venue} />
     {:else}
       <p class="text-center">You don't organize any venues, yet</p>
     {/each}
   </div>
-  <IconButton text="Create a venue" on:click={() => isSavingVenue = true}>
+  <IconButton text="Create a venue" on:click={() => isSavingModalShown = true}>
     <svg xmlns="http://www.w3.org/2000/svg" class="w-full h-full" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width={2} stroke-linecap="round" stroke-linejoin="round">
       <path d="M12 5v14M5 12h14"/>
     </svg>
   </IconButton>
 </div>
 
-<Modal
-  title={venueToModify ? 'Edit venue' : 'Create a venue'}
-  onRequestClose={() => isSavingVenue = false}
-  isOpen={isSavingVenue}
->
-  <SaveVenueForm venue={venueToModify} onSaved={() => isSavingVenue = false} />
+<Modal title="Create a venue" isOpen={isSavingModalShown} onRequestClose={() => isSavingModalShown = false}>
+  <SaveVenueForm onSaved={() => isSavingModalShown = false} />
 </Modal>
-
-{#if venueToModify}
-   <Modal title="Delete venue" isOpen={isDeletingVenue} onRequestClose={() => isDeletingVenue = false}>
-     <DeleteVenueForm venue={venueToModify} onDeleted={() => isDeletingVenue = false} />
-   </Modal>
-{/if}
