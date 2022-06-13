b70c3a10e85640be545c329e9c1ad4e5298ea7a2
davidfrp <davidfrp@outlook.dk>
2022-06-13 11:34:45 +0200

    Updated event service to handle saving events

--- client/src/lib/eventService.ts
+++ client/src/lib/eventService.ts
@@ -1,4 +1,4 @@
-import { get, patch } from '$lib/api'
+import { get, post, patch } from '$lib/api'
 
 export const getEvents = (fetch: typeof window.fetch) =>
   get('/events', fetch)
@@ -9,5 +9,14 @@ export const getEventsByVenue = (slug: string, fetch?: typeof window.fetch) =>
 export const getEventBySlug = (slug: string, fetch?: typeof window.fetch) =>
   get(`/events/${slug}`, fetch)
 
-export const saveEvent = ({ _id, id, ...event }: Record<string, unknown>) =>
-  patch(`/events/${_id ?? id}`, event, fetch)
+export const saveEvent = async ({ slug, ...event }: Record<string, unknown>) => {
+  let response: [Record<string, unknown>, 'error' | 'success']
+
+  if (slug) {
+    response = await patch(`/events/${slug}`, event, fetch)
+  } else {
+    response = await post('/events', event, fetch)
+  }
+
+  return response
+}
