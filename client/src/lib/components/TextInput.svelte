e2f59eae0add0b4e9910adbaf3d37d63546118b6
davidfrp <davidfrp@outlook.dk>
2022-06-14 22:39:38 +0200

    Added optional isRequired attribute for text inputs

--- client/src/lib/components/TextInput.svelte
+++ client/src/lib/components/TextInput.svelte
@@ -5,19 +5,26 @@
   export let value: string | undefined = undefined
   export let inputRef: HTMLInputElement | null = null
   export let isPassword: boolean = false
+  export let isRequired: boolean = false
 
   let isPasswordShown: boolean = false
 </script>
 
-<fieldset class="flex flex-col min-w-[20rem]">
+<fieldset class="flex flex-col">
   {#if label}
-    <label for={id} class="mb-2">{label}</label>
+    <label for={id} class="mb-2">
+      {label}
+      {#if isRequired}
+        <span title="This field is required" class="text-rose-600">*</span>
+      {/if}
+    </label>
   {/if}
   <div class="flex items-center relative">
     <input
       class="px-4 py-3 w-full bg-gray-100 rounded-lg border placeholder:text-gray-500"
       {...{type: isPassword && !isPasswordShown ? 'password' : 'text'}}
       placeholder={placeholder}
+      required={isRequired}
       bind:this={inputRef}
       bind:value={value}
       name={id}
