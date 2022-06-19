405a5da98d9803a511f8caa4e13737c5340b4016
davidfrp <davidfrp@outlook.dk>
2022-06-19 08:16:43 +0200

    Added dropdown selection component

---
+++ client/src/lib/components/DropdownSelection.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  export let id: string
+  export let label: string
+  export let value: any
+</script>
+
+<div class="flex flex-col">
+  <label for={id} class="mb-2">{label}</label>
+  <div class="px-4 w-full bg-gray-100 rounded-lg border">
+    <select
+      class="w-full py-3 bg-inherit cursor-pointer"
+      bind:value
+      {id}
+    >
+      <slot />
+    </select>
+  </div>
+</div>
