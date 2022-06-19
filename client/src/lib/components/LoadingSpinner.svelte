a820b9bb0b47bb5c60a91b8b3801d1c737c47e13
davidfrp <davidfrp@outlook.dk>
2022-06-19 08:35:48 +0200

    Deleted unused components

--- client/src/lib/components/LoadingSpinner.svelte
+++
@@ -1,3 +0,0 @@
-<div class="min-h-[calc(100vh-4rem)] flex items-center justify-center">
-  <div class="animate-spin h-14 w-14 rounded-full border-[7px] border-transparent border-r-pink-500"></div>
-</div>
