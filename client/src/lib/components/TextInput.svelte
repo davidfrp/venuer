876880647ea728a9215c686be12964ad70d0c9ca
davidfrp <davidfrp@outlook.dk>
2022-06-19 08:13:11 +0200

    Added support for multiple lines in text inputs

--- client/src/lib/components/TextInput.svelte
+++ client/src/lib/components/TextInput.svelte
@@ -4,6 +4,7 @@
   export let placeholder: string | undefined = undefined
   export let value: string | undefined = undefined
   export let inputRef: HTMLInputElement | null = null
+  export let isMultiline: boolean = false
   export let isPassword: boolean = false
   export let isRequired: boolean = false
 
@@ -20,28 +21,39 @@
     </label>
   {/if}
   <div class="flex items-center relative">
-    <input
-      class="px-4 py-3 w-full bg-gray-100 rounded-lg border placeholder:text-gray-500"
-      {...{type: isPassword && !isPasswordShown ? 'password' : 'text'}}
-      placeholder={placeholder}
-      required={isRequired}
-      bind:this={inputRef}
-      bind:value={value}
-      name={id}
-      id={id}
-    />
-    {#if isPassword}
-      <button
-        class="absolute right-4 font-semibold text-sm text-brand
-        hover:text-opacity-90 transition-colors"
-        on:click={() => isPasswordShown = !isPasswordShown}
-        aria-controls={id}
-        tabindex={-1}
-        type="button"
-        role="switch"
-      >
-        {isPasswordShown ? 'hide' : 'show'}
-      </button>
+    {#if isMultiline}
+      <textarea
+        class="px-4 py-3 w-full bg-gray-100 rounded-lg border resize-none placeholder:text-gray-500"
+        {placeholder}
+        bind:value
+        rows={4}
+        name={id}
+        {id}
+      />
+    {:else}
+      <input
+        class="px-4 py-3 w-full bg-gray-100 rounded-lg border placeholder:text-gray-500"
+        {...{type: isPassword && !isPasswordShown ? 'password' : 'text'}}
+        bind:this={inputRef}
+        bind:value
+        on:keydown
+        {placeholder}
+        name={id}
+        {id}
+      />
+      {#if isPassword}
+        <button
+          class="absolute right-4 font-semibold text-sm text-brand
+          hover:text-opacity-90 transition-colors"
+          on:click={() => isPasswordShown = !isPasswordShown}
+          aria-controls={id}
+          tabindex={-1}
+          type="button"
+          role="switch"
+        >
+          {isPasswordShown ? 'hide' : 'show'}
+        </button>
+      {/if}
     {/if}
   </div>
 </fieldset>
