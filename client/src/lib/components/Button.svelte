1355a310775d89e4411c7c7dcb6b8494725a55ee
davidfrp <davidfrp@outlook.dk>
2022-06-19 18:45:10 +0200

    Reverted white background fix, added floating button type

--- client/src/lib/components/Button.svelte
+++ client/src/lib/components/Button.svelte
@@ -8,10 +8,13 @@
     )
   )
 
+  $: floatingStyles = isFloating ? `absolute bottom-0 left-0 right-0 sm:right-auto
+    sm:left-1/2 sm:-translate-x-1/2 w-full h-14 min-w-[4rem] sm:w-auto sm:h-auto` : ''
+
   $: baseStyles = variant !== 'none' ? `relative w-full h-full
     flex justify-center items-center leading-5 text-opacity-[0.85] 
     font-semibold bg-brand rounded-lg transition-colors 
-    whitespace-nowrap ${sizeStyles}` : ''
+    whitespace-nowrap ${floatingStyles} ${sizeStyles}` : ''
 
   $: containedStyles = variant === 'contained' ? `text-white text-opacity-100 
     bg-opacity-100 hover:bg-opacity-90` : ''
@@ -32,16 +35,15 @@
   export let size: 'sm' | 'base' | 'md' | 'lg' = 'base'
   export let isDisabled: boolean = false
   export let isLoading: boolean = false
+  export let isFloating: boolean = false
   export let focusRef: HTMLButtonElement | null = null
 </script>
 
-<div class="rounded-lg bg-white">
-  <button {type} class={styles} on:click bind:this={focusRef} tabindex={isDisabled ? -1 : 0}>
-    {#if isLoading}
-      <div class="absolute animate-spin h-7 w-7 rounded-full border-4 border-transparent border-r-white"></div>
-    {/if}
-    <div class:invisible={isLoading}>
-      <slot />
-    </div>
-  </button>
-</div>
+<button {type} class={styles} on:click bind:this={focusRef} tabindex={isDisabled ? -1 : 0}>
+  {#if isLoading}
+    <div class="absolute animate-spin h-7 w-7 rounded-full border-4 border-transparent border-r-white"></div>
+  {/if}
+  <div class:invisible={isLoading}>
+    <slot />
+  </div>
+</button>
