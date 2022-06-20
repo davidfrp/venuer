4c80b70e65309ffc2f264b4fc03eb91c50a07df2
davidfrp <davidfrp@outlook.dk>
2022-06-20 10:16:39 +0200

    Event cards now show a badge if the event is organized by the user

--- client/src/routes/_EventCard.svelte
+++ client/src/routes/_EventCard.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
+  import { session } from '$app/stores'
   import dayjs from 'dayjs'
+
   export let event: VenueEvent
 </script>
 
@@ -8,6 +10,14 @@
     <img src={event.imageUrl} alt="" class="w-full h-full object-cover">
   </div>
   <div class="py-3">
+    {#if event.venue.organizer === $session.user?._id}
+      <span class="px-2 py-0.5 mb-1 inline-block rounded-full 
+        whitespace-nowrap bg-brand bg-opacity-10 text-brand 
+        text-xs font-semibold uppercase tracking-wide"
+      >
+        Your event
+      </span>
+    {/if}
     <h1 class="text-lg font-semibold truncate">{event.name}</h1>
     <div class="text-sm text-gray-500">
       {event.venue.location.city}, {event.venue.name}
